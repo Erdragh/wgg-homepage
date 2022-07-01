bdd549a934268b4b79fd55b2fa90ec928d31e01d
Erdragh <enderjanyt@gmail.com>
2022-07-01 11:05:20 +0200

    added slight display options to formulae

--- resources/js/Components/Formula.vue
+++ resources/js/Components/Formula.vue
@@ -1,6 +1,10 @@
 <template>
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.16.0/dist/katex.min.css" integrity="sha384-Xi8rHCmBmhbuyyhbI88391ZKP2dmfnOl4rT9ZfRI7mLTdk1wblIUnrIq35nqwEvC" crossorigin="anonymous">
-<div class="formula" ref="formula"><slot/></div>
+<div class="container">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.16.0/dist/katex.min.css" integrity="sha384-Xi8rHCmBmhbuyyhbI88391ZKP2dmfnOl4rT9ZfRI7mLTdk1wblIUnrIq35nqwEvC" crossorigin="anonymous">
+	<span ref="formula">
+		<slot/>
+	</span>
+</div>
 </template>
 
 <script>
@@ -16,5 +20,25 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+div.container {
+	padding: 10px;
+	width: max-content;
+	margin-inline: auto;
+	&.left {
+		// text-align: center;
+		margin-left: 0;
+		&.float {
+			margin-right: 0;
+			float: left;
+		}
+	}
+	&.right {
+		// text-align: right;
+		margin-right: 0;
+		&.float {
+			margin-left: 0;
+			float: right;
+		}
+	}
+}
 </style>
