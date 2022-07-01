05a56803a5d91b60cff917e08aff8289cfac4cf7
Erdragh <enderjanyt@gmail.com>
2022-07-01 11:19:34 +0200

    added the ability to add subtext to a formula

--- resources/js/Components/Formula.vue
+++ resources/js/Components/Formula.vue
@@ -1,9 +1,12 @@
 <template>
 <div class="container">
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.16.0/dist/katex.min.css" integrity="sha384-Xi8rHCmBmhbuyyhbI88391ZKP2dmfnOl4rT9ZfRI7mLTdk1wblIUnrIq35nqwEvC" crossorigin="anonymous">
-	<span ref="formula">
+	<span class="formula" ref="formula">
 		<slot/>
 	</span>
+	<div v-if="subtext" class="subtext">
+		{{ subtext }}
+	</div>
 </div>
 </template>
 
@@ -11,6 +14,7 @@
 import katex from 'katex';
 
 export default {
+	props: ['subtext'],
 	mounted() {
 		katex.render(this.$refs.formula.innerHTML, this.$refs.formula, {
 			throwOnError: false
@@ -24,6 +28,7 @@ div.container {
 	padding: 10px;
 	width: max-content;
 	margin-inline: auto;
+	text-align: center;
 	&.left {
 		// text-align: center;
 		margin-left: 0;
@@ -38,6 +43,30 @@ div.container {
 		&.float {
 			margin-left: 0;
 			float: right;
+		}
+	}
+	// span.formula {
+	// 	margin: 0 auto;
+	// }
+	div.subtext {
+		display: block;
+		text-align: center;
+		font-style: italic;
+		opacity: .8;
+		font-size: .8em;
+		margin-top: 10px;
+		padding-top: 5px;
+		position: relative;
+		&:before {
+			content: '';
+			display: block;
+			position: absolute;
+			top: 0;
+			left: 0;
+			width: 100%;
+			height: 1px;
+			background: var(--clr-font);
+			opacity: .3;
 		}
 	}
 }
